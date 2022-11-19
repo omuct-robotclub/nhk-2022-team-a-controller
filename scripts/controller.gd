df50fc2de70cd7404bcd70c2df6b4a7adef2f702
Pylgos <43234674+Pylgos@users.noreply.github.com>
2022-11-19 10:38:50 +0900

    add haptics feedback

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -33,6 +33,11 @@ func _process(_delta: float) -> void:
         robot.set_velocity(linear, angular)
         _last_twist_publish_time = now
 
+func _feedback() -> void:
+    for i in Input.get_connected_joypads():
+        Input.start_joy_vibration(i, 1, 0, 0.1)
+    Input.vibrate_handheld(50)
+
 func _input(_event: InputEvent) -> void:
     if Input.is_action_just_pressed("step_turn_left"):
         robot.move_to(robot.position, robot.rotation + deg2rad(0.5))
@@ -42,20 +47,25 @@ func _input(_event: InputEvent) -> void:
     if Input.is_action_just_pressed("fire_0"):
         _selected_launcher = 0
         emit_signal("launcher_selected")
+        _feedback()
     elif Input.is_action_just_pressed("fire_1"):
         _selected_launcher = 1
         emit_signal("launcher_selected")
+        _feedback()
     elif Input.is_action_just_pressed("fire_2"):
         _selected_launcher = 2
         emit_signal("launcher_selected")
+        _feedback()
 
     if Input.is_action_just_pressed("reload_mod"):
         if _selected_launcher != -1:
             robot.get_loader(_selected_launcher).reload()
+            _feedback()
 
     if Input.is_action_just_pressed("fire"):
         if _selected_launcher != -1:
             robot.get_launcher(_selected_launcher).launch()
+            _feedback()
 
 
 func get_selected_launcher() -> int:
